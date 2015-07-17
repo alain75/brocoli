9fe88b7df2bd8cf87c895df24be5486ec2729e3f
alain75 <tux7@hotmail.fr>
2015-07-17 10:52:12 +0200

    add contents and some headers

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!doctype html>
 <html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
 	<title>index</title>
 </head>
 <body>
-	
+	<h1>GitHub</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur 
+		adipisicing elit. Et, aliquid, accusamus, 
+		nam dolorum soluta voluptatem saepe 
+		voluptatibus ipsam cum laudantium 
+		doloribus aspernatur maiores earum dignissimos 
+		asperiores nobis enim delectus autem.</p>
 </body>
 </html>
